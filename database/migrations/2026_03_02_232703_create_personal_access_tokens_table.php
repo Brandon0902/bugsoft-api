b252cb352c8fa6dfea5ea40d200deb3377b61e02
Brandon0902 <brandonaxellopezdealba@gmail.com>
2026-03-02 17:37:25 -0600

    migración ducplicada

--- database/migrations/2026_03_02_232703_create_personal_access_tokens_table.php
+++ database/migrations/2026_03_02_232703_create_personal_access_tokens_table.php
@@ -6,17 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table): void {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('name');
+            $table->morphs('tokenable');
+            $table->text('name');
             $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable()->index();
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('personal_access_tokens');
